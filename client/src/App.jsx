8157975f04eeeee68b10a8f603506b97d59f1107
[Aqib abass] <aqibabass7@gmail.com>
2025-02-06 01:19:47 +0530

    now the imags are visible and the server doesnt crash

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
@@ -13,18 +13,24 @@ import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 import Hero from "./components/Hero";
 import CreateTrip from "./create-trip";
-import Viewtrip from "./view-trip/[tripId]/index.jsx";
+import ViewTrip from "./view-trip/[tripId]/index.jsx";
 import MyTrips from "./my-trips";
 import { Toaster } from "./components/ui/sonner";
 
-
-axios.defaults.baseURL = 'http://localhost:4000';
-axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:4000'; 
 
 function App() {
+  const location = useLocation();
+
+  // Conditional withCredntials
+  if (location.pathname === '/view-trip' || location.pathname.startsWith('/my-trips')) {
+    axios.defaults.withCredentials = false;
+  } else {
+    axios.defaults.withCredentials = true;
+  }
+
   return (
     <UserContextProvider>
-      
       <Toaster />
       <Routes>
         <Route path="/" element={<Layout />}>
@@ -41,7 +47,7 @@ function App() {
           <Route path="/account/bookings/:id" element={<BookingPage />} />
 
           <Route path="/create-trip" element={<CreateTrip />} />
-          <Route path="/view-trip/:tripId" element={<Viewtrip />} />
+          <Route path="/view-trip/:tripId" element={<ViewTrip />} />
           <Route path="/my-trips" element={<MyTrips />} />
         </Route>
       </Routes>
