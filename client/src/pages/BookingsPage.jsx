411fd6f117e152b44ce6f259557f7d7a436661cf
[Aqib abass] <aqibabass7@gmail.com>
2025-02-17 21:04:51 +0530

    trying  to fix booking page

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -13,7 +13,7 @@ function BookingsPage() {
     const fetchBookings = async () => {
       setLoading(true);
       try {
-        const response = await axios.get('/bookings');
+        const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/bookings`); // Corrected URL
         setBookings(response.data);
         setLoading(false);
       } catch (error) {
@@ -27,7 +27,7 @@ function BookingsPage() {
   const cancelBooking = async (id) => {
     try {
       setBookings(prevBookings => prevBookings.filter(booking => booking._id !== id));
-      await axios.delete(`/bookings/${id}`);
+      await axios.delete(`${import.meta.env.VITE_BASE_URL}/bookings/${id}`); // Corrected URL
     } catch (error) {
       setBookings(prevBookings => [...prevBookings, { _id: id }]);
     }
@@ -41,7 +41,6 @@ function BookingsPage() {
           <div>Loading...</div>
         ) : bookings.length === 0 ? (
           <div className="items-center text-xl font-bold mb-4">No bookings available</div>
-          
         ) : (
           bookings.map(booking => (
             <Link
