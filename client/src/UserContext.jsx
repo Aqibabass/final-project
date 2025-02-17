a7fb6c18a0eb7f51884c163c7fc6c82732b88602
[Aqib abass] <aqibabass7@gmail.com>
2025-02-18 00:32:54 +0530

    changed usercontext inorder to fix cors

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,6 +1,19 @@
 import axios from "axios";
 import { useEffect, createContext, useState } from "react";
 
+// Function to get user data from request
+function getUserDataFromReq(req) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+        if (err) {
+          reject(err); // Properly handle the error by calling reject
+        } else {
+          resolve(userData);
+        }
+      });
+    });
+  }
+
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
@@ -8,14 +21,18 @@ export function UserContextProvider({ children }) {
     const [ready, setReady] = useState(false);
 
     useEffect(() => {
-        axios.get('/profile')
-            .then(({ data }) => {
-                setUser(data);
+        const fetchProfile = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/profile`);
+                setUser(response.data);
                 setReady(true);
-            })
-            .catch(() => {
+            } catch (error) {
+                console.error('Error fetching profile:', error);
                 setReady(true);
-            });
+            }
+        };
+
+        fetchProfile();
     }, []);
 
     const updateUser = (userData) => {
